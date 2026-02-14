0849319a1df08cb33e63fa36715c5c719b9eaad9
kopyie <73689745+kopyie@users.noreply.github.com>
2026-02-14 14:04:38 +0630

    light

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import concurrent.futures
 import json
 
 # --- CONFIGURATION ---
-SOURCE_URL = "https://raw.githubusercontent.com/ebrasha/free-v2ray-public-list/refs/heads/main/V2Ray-Config-By-EbraSha-All-Type.txt"
+SOURCE_URL = "https://raw.githubusercontent.com/ebrasha/free-v2ray-public-list/refs/heads/main/V2Ray-Config-By-EbraSha.txt"
 OUTPUT_FILE = "sub.txt"
 TOP_N = 40          # Keep top 40 best servers
 TIMEOUT = 2         # 2 seconds max latency
